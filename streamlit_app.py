cdaadd2f5b4c8a6c6421809ab0d0ec9bb7df53f7
jamneeru <neerajjamwal184@gmail.com>
2025-11-12 10:03:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
-import snowflake.connector
-#from snowflake.snowpark.context import get_active_session
+#import snowflake.connector
 from snowflake.snowpark.functions import col, when_matched
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -45,4 +44,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
-
